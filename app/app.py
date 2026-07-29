989ec087640de09ad39480767c9735c20719b9c0
Karthik-2500031113 <2500031113@kluniversity.in>
2026-07-29 12:46:59 +0530

    Basic Web application is created

--- app/app.py
+++ app/app.py
@@ -2,29 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def home():
+def index():
     return render_template("home.html")
+
 
 @app.route("/dataset")
 def dataset():
-    return "<h2>Dataset Page</h2>"
+    return render_template("dataset.html")
+
 
 @app.route("/eda")
 def eda():
-    return "<h2>EDA Page</h2>"
+    return render_template("eda.html")
+
 
 @app.route("/preprocessing")
 def preprocessing():
-    return "<h2>Preprocessing Page</h2>"
+    return render_template("preprocessing.html")
+
 
 @app.route("/models")
 def models():
-    return "<h2>Models Page</h2>"
+    return render_template("models.html")
+
 
 @app.route("/results")
 def results():
-    return "<h2>Results Page</h2>"
+    return render_template("results.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True
+    )
