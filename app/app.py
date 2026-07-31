74ebfadf272f49b80e8a6519b1f87981769541a0
Karthik-2500031113 <2500031113@kluniversity.in>
2026-07-31 15:27:03 +0530

    Dataset is added in the project

--- app/app.py
+++ app/app.py
@@ -1,30 +1,24 @@
+from axios.navigator import first
 from flask import Flask, render_template
 
-app = Flask(__name__)
+from data.load_data import load_data, get_summary
 
+app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/dataset")
 def dataset():
-    return "<h2>Dataset Page</h2>"
+    df=load_data()
+    summary=get_summary(df)
+    return render_template(
+        "dataset.html",
+        summary=summary,
+        first_rows=df.head().to_html(index=False)
+    )
 
-@app.route("/eda")
-def eda():
-    return "<h2>EDA Page</h2>"
-
-@app.route("/preprocessing")
-def preprocessing():
-    return "<h2>Preprocessing Page</h2>"
-
-@app.route("/models")
-def models():
-    return "<h2>Models Page</h2>"
-
-@app.route("/results")
-def results():
-    return "<h2>Results Page</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
